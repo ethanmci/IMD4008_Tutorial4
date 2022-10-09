8737f1f723e87841bdf16254faf6a49a5289a3aa
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2022-10-09 02:48:21 -0400

    Initial commit

---
+++ app/src/main/java/com/example/imd4008_tutorial4/ui/settings/SettingsViewModel.java
@@ -0,0 +1,7 @@
+package com.example.imd4008_tutorial4.ui.settings;
+
+import androidx.lifecycle.ViewModel;
+
+public class SettingsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
