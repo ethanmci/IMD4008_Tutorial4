3d066fd4b9a4a61f2c7399c51c6572b3ed8c4993
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2022-10-10 03:38:31 -0400

    counter works, no persist tho

--- app/src/main/java/com/example/imd4008_tutorial4/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/imd4008_tutorial4/ui/dashboard/DashboardFragment.java
@@ -4,17 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.imd4008_tutorial4.R;
 import com.example.imd4008_tutorial4.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+
+    private TextView counter;
+    private Button increment;
+    int count = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +30,17 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        counter = root.findViewById(R.id.dashboard_counter);
+        increment = root.findViewById(R.id.dashboard_button);
+
+        increment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                counter.setText(String.valueOf(count));
+            }
+        });
+
         return root;
     }
 
--- app/src/main/java/com/example/imd4008_tutorial4/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/imd4008_tutorial4/ui/notifications/NotificationsFragment.java
@@ -4,17 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.imd4008_tutorial4.R;
 import com.example.imd4008_tutorial4.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+    private TextView counter;
+    private Button increment;
+    int count = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +29,17 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        counter = root.findViewById(R.id.notification_counter);
+        increment = root.findViewById(R.id.notification_button);
+
+        increment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                counter.setText(String.valueOf(count));
+            }
+        });
+
         return root;
     }
 
